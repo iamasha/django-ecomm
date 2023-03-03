2f0d7da9afb9923a0939fcaa07431a8f2011ba8a
asha <asha.hwaka@tmcg.co.ug>
2023-03-03 05:03:35 +0300

    modify field for price

--- backend/backendapi/store/models.py
+++ backend/backendapi/store/models.py
@@ -11,7 +11,7 @@ class Customer(models.Model):
         return self.name
 class Product(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
     digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
